3249abedcf21e611d82de539f76d48df864fd1bd
bio-boris <bio-boris@users.noreply.github.com>
2018-12-16 00:58:04 -0600

    Update telegraf.sh

--- telegraf.sh
+++ telegraf.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 sed "s#http://18.222.179.234:8086#http://$1:8086#g" /etc/telegraf/telegraf.conf > telegraf.conf
 sudo cp telegraf.conf /etc/telegraf/telegraf.conf
-sudo systemctl status telegraf > telegraf.before.log
-sudo systemctl restart telegraf 
-sudo systemctl reload telegraf
-sudo systemctl status telegraf > telegraf.log
+sudo service telegraf stop
+sudo service telegraf start
+sudo service telegraf reload 
+
+sudo service telegraf status > telegraf_service_status.log
+sudo systemctl status telegraf > telegraf_systemctl_status.log
