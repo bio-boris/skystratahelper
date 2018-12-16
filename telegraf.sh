6cf5f16279deeb46dc02aa11d37b662fdfec3dc5
bio-boris <bio-boris@users.noreply.github.com>
2018-12-16 00:50:16 -0600

    Update telegraf.sh

--- telegraf.sh
+++ telegraf.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 sed "s#http://18.222.179.234:8086#http://$1:8086#g" /etc/telegraf/telegraf.conf > telegraf.conf
 sudo cp telegraf.conf /etc/telegraf/telegraf.conf
+sudo systemctl status telegraf > telegraf.before.log
 sudo systemctl restart telegraf 
 sudo systemctl reload telegraf
 sudo systemctl status telegraf > telegraf.log
